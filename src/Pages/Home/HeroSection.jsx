d1aef21e43bfcb91edeb9e55aa1e4aa3f7cdaf46
MohanVaddella <mohanvaddella@gmail.com>
2024-01-09 13:15:29 +0530

    creating hero section

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,17 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section-content">
+            <p className="section--title">Hey, I'm Mohan</p>
+            
+            <span className="hero--section-title--color">
+  I am an enthusiastic techie specializing in Cloud Computing and Web Development, demonstrating proficiency in designing and implementing innovative solutions.
+</span>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img"><img src=</div>
+    </section>
+  );
+}
