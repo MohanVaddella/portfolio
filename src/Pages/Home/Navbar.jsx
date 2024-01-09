e4190d772f4bd5d11b40c1af3852e06247f579fa
MohanVaddella <mohanvaddella@gmail.com>
2024-01-09 20:36:13 +0530

    Navbar.jsx in progress

---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from React;
+import { Link } from "react-scroll";
+
+function Navbar() {
+    const [navActive, setNavActive] = useState(false);
+    const toggleNav = () => {
+        setNavActive(!navActive)
+    }
+
+    const closeMenu = {}
+}
