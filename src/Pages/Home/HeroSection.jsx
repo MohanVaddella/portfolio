67471ade60fb65254371e795f6fd3e6fc8428774
MohanVaddella <mohanvaddella@gmail.com>
2024-01-09 19:45:27 +0530

    herosection completed

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -2,11 +2,11 @@ export default function HeroSection() {
   return (
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
-        <div className="hero--section-content">
-            <p className="section--title">Hey, I'm Mohan</p>
+        <div className="hero--section--content">
+            <p className="hero--section--title">Hey, I'm Mohan</p>
             
-            <span className="hero--section-title--color">
-  I am an enthusiastic techie specializing in Cloud Computing and Web Development, demonstrating proficiency in designing and implementing innovative solutions.
+            <span className="hero--section-description">
+  I am an enthusiastic techie specializing in <b>Cloud Computing</b> and <b>Web Development</b>, demonstrating proficiency in designing and implementing innovative solutions.
 </span>
         </div>
         <button className="btn btn-primary">Get In Touch</button>
